42fa91083bab9f0dee01a156e14dcf948b6fceb8
rkven <99968975+rkven@users.noreply.github.com>
2023-09-27 22:25:45 +0530

    Add files via upload

---
+++ math_calculations.py
@@ -0,0 +1,31 @@
+# Basic functionsfor math calculations
+
+
+def addition(num1,num2):
+    x= float(num1)
+    y=float(num2)
+    print (x + y)
+
+def division(num1,num2):
+    x= float(num1)
+    y=float(num2)
+    print(x/y)
+
+def div_reminder(num1,num2):
+    x= float(num1)
+    y=float(num2)
+    print(x%y)
+    
+def multiplication(num1,num2):
+    x= float(num1)
+    y=float(num2)
+    print(x*y)
+
+def power(num1, exponent):
+    x=float(num1)
+    print(x**exponent)
+
+def floor_division(num1,num2):
+    x= float(num1)
+    y=float(num2)
+    print(x//y)
